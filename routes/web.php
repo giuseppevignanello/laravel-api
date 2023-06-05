77f8ed93b731228d9e9dbd3f6b6758c076e1fbbe
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-05 15:17:45 +0200

    add route group

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', [DashboardController::class, 'index'])->name(('dashboard'));
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name(('dashboard'));
 });
 
 require __DIR__ . '/auth.php';
