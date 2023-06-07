df96ff533af352dc27bf4118d676e581e88ac9e0
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-07 15:04:17 +0200

    add one to many connection between types and projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
+use App\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description', 'status', 'duration', 'start_date', 'end_date', 'slug', 'repo_link', 'view_link'];
+    protected $fillable = ['title', 'description', 'status', 'duration', 'start_date', 'end_date', 'slug', 'repo_link', 'view_link', 'type_id'];
 
     public static function generateSlug($title)
     {
         return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
