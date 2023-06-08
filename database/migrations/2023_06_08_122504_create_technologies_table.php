dac8c93890699a7903c3786bef2bb5f1213fb6be
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-08 14:39:10 +0200

    add technologies migration and Technology Model

--- database/migrations/2023_06_08_122504_create_technologies_table.php
+++ database/migrations/2023_06_08_122504_create_technologies_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('types', function (Blueprint $table) {
+        Schema::create('technologies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('types');
+        Schema::dropIfExists('technologies');
     }
 };
