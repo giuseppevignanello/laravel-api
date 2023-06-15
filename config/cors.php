fcd59271e722f88636c4a8e1668f52077f243ff9
giuseppevignanello <peppe.vignanello@gmail.com>
2023-06-15 12:06:56 +0200

    CHANGE cors permissions

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => [env('APP_FRONTEND_URL', 'http://localhost:5174/')],
 
     'allowed_origins' => ['*'],
 
